b92e6ae9c519edfc3ffc2fcb48764d97eb328e8b
SamBonnekamp <18bonnekampsb2@webmail.sis.edu.hk>
2017-06-21 22:37:18 +0800

    Uplaod 2

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("105:1 br 25:2 br 62:3 br 30:1 br 142:5 br 90:7 br 5:2 br 106:1")
+/* Type JavaScript below */
+var cruz = document.getElementById("cruz");
+var GS = document.getElementById("GS");
+var distance = document.getElementById("dist");
+var oil = document.getElementById("fuel");
+var tod = document.getElementById("tod");
+var vertSpeed = document.getElementById("verts");
+var fuelNeeded = document.getElementById("fuelneed");
+
+// <-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- >
+
+function ToD() {
+  var desc = cruz.value / 10 * 3 + 10;
+  tod.innerHTML = Math.floor(desc) + " nm";
+  // alert("hello");
+}
+
+function vS() {
+  vertSpeed.innerHTML = GS.value / 2 * 10 + " ft/min";
+}
+
+function fuelNeed() {
+  var fuel = distance.value / GS.value * oil.value * 2 + 12000 + 2000;
+  fuelNeeded.innerHTML = Math.floor(fuel) + " lbs";
+}
+
+function start() {
+  ToD();
+  vS();
+  fuelNeed();
+}
